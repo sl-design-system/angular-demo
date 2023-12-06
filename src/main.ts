82c4598ad9bc9e1574f5f3a98aae27415167d383
Diana Broeders <diana.broeders@sanoma.com>
2023-12-06 17:21:38 +0100

    start using sl-button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { setup } from '@sl-design-system/sanoma-learning'; 
+import '@sl-design-system/button/register.js'; 
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+setup();
