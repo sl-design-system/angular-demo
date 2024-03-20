3fad1ca2d2d9881d7fa38a21be446022015d7b40
anna-lach <a.lach@nowaera.pl>
2024-03-20 17:38:27 +0100

    Trying to use SLDS forms in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SearchComponent } from './search/search.component';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+// import {FormsModule} from "@sl-design-system/angular";
+import { FormsModule as CoreFormsModule } from '@sl-design-system/angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SearchComponent],
+  imports: [CommonModule, RouterOutlet, SearchComponent, ReactiveFormsModule, CoreFormsModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,4 +21,12 @@ export class AppComponent {
   search(searchForm:{query:string}) {
     this.query = searchForm.query;
   }
+
+  formGroup = new FormGroup({
+    textField: new FormControl('Text field'),
+    checkbox: new FormControl('checked'),
+    select: new FormControl('1'),
+    checkboxGroup: new FormControl(['2', '1', '0']),
+    radioGroup: new FormControl('1')
+  });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { setup } from '@sl-design-system/sanoma-learning'; 
-import '@sl-design-system/button/register.js'; 
+import { setup } from '@sl-design-system/sanoma-learning';
+import '@oddbird/popover-polyfill';
+import '@webcomponents/scoped-custom-element-registry/scoped-custom-element-registry.min.js';
+import '@sl-design-system/button/register.js';
+import '@sl-design-system/icon/register.js';
+import '@sl-design-system/checkbox/register.js';
+import '@sl-design-system/form/register.js';
+import '@sl-design-system/radio-group/register.js';
+import '@sl-design-system/text-field/register.js';
+import '@sl-design-system/textarea/register.js';
+import '@sl-design-system/select/register.js';
+import '@sl-design-system/switch/register.js';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
