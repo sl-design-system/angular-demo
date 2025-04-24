37b55a7e603a0b4bb2cfcacafc50eddc28e4a616
anna-lach <a.lach@nowaera.pl>
2025-04-24 10:08:49 +0200

    cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,25 +9,5 @@ import '@sl-design-system/checklist';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tabs = [
-    { path: '/form', label: 'form' },
-    { path: '/icons', label: 'icons' },
-    { path: '/dashboard', label: 'Dashboard' },
-    { path: '/assignments', label: 'Assignments' },
-    { path: '/grades', label: 'Grades' },
-    { path: '/schedule', label: 'Schedule' },
-    { path: '/resources', label: 'Resources' },
-    { path: '/announcements', label: 'Announcements' },
-    { path: '/profile', label: 'Profile' },
-    { path: '/settings', label: 'Settings' },
-    { path: '/support', label: 'Support' },
-    { path: '/form', label: 'form' },
-    { path: '/icons', label: 'icons' }
-  ];
-
   constructor( ) {}
-
-  ngOnInit() {
-    console.log('tabs in ngOnInit', this.tabs);
-  }
 }
